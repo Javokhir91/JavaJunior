e9ac067ee1d0e66ebdffa17ed019fa4b09da0e2f
Djava <91669347+Javokhir91@users.noreply.github.com>
2024-04-24 02:48:47 +0300

    Add files via upload
    Ключи вынес в отдельный класс чтобы якобы обезопасить !)

---
+++ lesson3/Keeys.java
@@ -0,0 +1,8 @@
+package ru.JavaJunior.lesson3;
+
+public class Keeys {
+
+    public static final String URL = "jdbc:mysql://localhost:3306";
+    public static final String USER = "root";
+    public static final String PASSWORD = "12345678";
+}
